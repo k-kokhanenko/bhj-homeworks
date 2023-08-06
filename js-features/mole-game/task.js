1ad53cadd6ac5745bb629bd3c5d9b6886f840caa
k-kokhanenko <k.kokhanenko@yandex.ru>
2023-08-06 14:02:14 +0300

    some add

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -3,24 +3,25 @@ let elements = document.querySelectorAll(".hole");
 
 for (let element of elements) {
     element.addEventListener("click", function(){
-        function clear(dead, lost) {
+        function clear() {
+            const dead = document.getElementById("dead");
+            const lost = document.getElementById("lost");
+    
             dead.textContent = 0;
             lost.textContent = 0;
         }
 
-        const dead = document.getElementById("dead");
-        const lost = document.getElementById("lost");
         if (this.classList.contains("hole_has-mole")) {
             dead.textContent++; 
             if (dead.textContent >= 10) {
                 alert("Победа");
-                clear(dead, lost);
+                clear();
             }
         } else {
             lost.textContent++;
             if (lost.textContent >= 5) {
                 alert("Вы проиграли");
-                clear(dead, lost);
+                clear();
             }
         }
     });
