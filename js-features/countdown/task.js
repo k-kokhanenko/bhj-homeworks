ce33e1984c5fdd16b479ad164aa8b8a50d205e48
k-kokhanenko <k.kokhanenko@yandex.ru>
2023-08-06 12:52:42 +0300

    first

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,12 +1,31 @@
 const timer = document.getElementById("timer");
 
+/*
 let timerId = setInterval(
     () => {
         timer.textContent -= 1;
         
 
-        if (timer == 0) {
+        if (timer.textContent == 0) {
             alert("Вы победили в конкурсе!");
             clearInterval(timerId);
         }
     }, 1000);
+*/
+
+// #1
+const currentDate = new Date();
+timer.textContent = `{currentDate.getHours()}:{currentDate.getMinutes()}:{currentDate.getSeconds()}`;
+
+let timerId = setInterval(
+    () => {
+        currentDate.setSeconds(currentDate.getSeconds() - 1);
+        timer.textContent = `{currentDate.getHours()}:{currentDate.getMinutes()}:{currentDate.getSeconds()}`;
+
+        if (currentDate.getTime() == 0) {
+            alert("Вы победили в конкурсе!");
+            clearInterval(timerId);
+        }
+    }, 1000);
+
+
