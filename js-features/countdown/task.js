a909e69835ac7f9b4c358dfb6d626cac838deabc
k-kokhanenko <k.kokhanenko@yandex.ru>
2023-08-06 12:57:41 +0300

    some fix

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -6,26 +6,25 @@ let timerId = setInterval(
         timer.textContent -= 1;
         
 
-        if (timer.textContent == 0) {
-            alert("Вы победили в конкурсе!");
+        if (timer.textContent == 0) {            
             clearInterval(timerId);
+            alert("Вы победили в конкурсе!");
+            //window.location = "some address here";
         }
     }, 1000);
 */
 
 // #1
 const currentDate = new Date();
-timer.textContent = `{currentDate.getHours()}:{currentDate.getMinutes()}:{currentDate.getSeconds()}`;
+timer.textContent = `${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
 
 let timerId = setInterval(
     () => {
         currentDate.setSeconds(currentDate.getSeconds() - 1);
-        timer.textContent = `{currentDate.getHours()}:{currentDate.getMinutes()}:{currentDate.getSeconds()}`;
+        timer.textContent = `${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
 
         if (currentDate.getTime() == 0) {
             alert("Вы победили в конкурсе!");
             clearInterval(timerId);
         }
     }, 1000);
-
-
