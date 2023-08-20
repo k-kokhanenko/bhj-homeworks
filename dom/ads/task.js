04649583086d04a7445cd95b27ac02a41bb5a5c4
k-kokhanenko <k.kokhanenko@yandex.ru>
2023-08-20 13:06:50 +0300

    initial commit

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,5 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
+    let mode = 0;
+    let interval = 1000;
+    const elements = document.querySelectorAll(".rotator .rotator__case");
+
     setInterval(() => {
-                    
-    }, 1000);    
+        const activeElement = document.querySelector(".rotator .rotator__case_active");  
+
+        let nextElement = null;
+        if (mode == 0) {
+            nextElement = activeElement.nextElementSibling;
+            if (!nextElement) {
+                mode = 1;
+                nextElement = activeElement.previousElementSibling;
+            }
+        } else {
+            nextElement = activeElement.previousElementSibling;
+            if (!nextElement) {
+                mode = 0;
+                nextElement = activeElement.nextElementSibling;
+            }
+        }
+                   
+        activeElement.classList.remove("rotator__case_active");
+        nextElement.classList.add("rotator__case_active");
+        nextElement.style = `color : ${nextElement.dataset.color}`;
+        interval = activeElement.dataset.speed;
+    }, interval);    
 });
