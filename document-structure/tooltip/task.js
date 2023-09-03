2253f1ce50de3f4a8ea60d2cf84761f08644b040
k-kokhanenko <k.kokhanenko@yandex.ru>
2023-09-03 17:21:35 +0300

    complete

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,21 +1,27 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-
-    const div = document.createElement('div');
-    div.classList.add('tooltip');    
-    //document.body.appendChild(div);
-    //document.body.insertBefore(div, document.body.firstChild);
-    
-    document.querySelectorAll('.has-tooltip').forEach(function(item) {        
+    document.querySelectorAll('.has-tooltip').forEach(function(item) {    
+        const div = document.createElement('div');
+        div.classList.add('tooltip');        
+        item.parentNode.insertBefore(div, item.nextSibling);
+        
         item.addEventListener( "click" , (event) => {
             event.preventDefault();
-
-            div.textContent = item.title;
-            div.classList.add('tooltip_active');
-            //console.log(div);
-
-            item.appendChild(div);
+            
+            document.querySelectorAll('.tooltip_active').forEach(function(el) {   
+                el.classList.remove('tooltip_active');
+            }); 
+            
+            let rect = item.getBoundingClientRect();          
+            const tooltip = item.nextElementSibling;
+            if (tooltip) {
+                tooltip.textContent = item.title;
+                tooltip.style.cssText = `left: ${rect.x}px; top: ${rect.y+rect.height}px;`;
+                tooltip.classList.toggle('tooltip_active');     
+            }
         });
     });
 });
+
+
 
